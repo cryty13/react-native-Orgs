78a4e222d7743f053b18f947a8ce677225774330
Willian <willian.buqui@gmail.com>
2022-04-21 22:51:45 -0300

    ajustado para fazer o curso

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
-import react from 'react'; 
-import { StatusBar, SafeAreaView} from 'react-native';
-import { View } from 'react-native';
-import { 
-  useFonts,
-  Montserrat_400Regular, 
-  Montserrat_700Bold
-} from '@expo-google-fonts/montserrat';
-import AppLoading from 'expo-app-loading';
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
+import useProdutores from './src/hooks/useProdutores';
 
+import Home from './src/telas/Home';
 import Cesta from './src/telas/Cesta';
-import mock from './src/mocks/cesta'
 
 export default function App() {
+  const produtores = useProdutores(false);
 
-  const [fonteCarregada] = useFonts({
-    "MontserratRegular":Montserrat_400Regular,
-    "MontserratBold":Montserrat_700Bold,
-  })
-
-  if(!fonteCarregada){
-    return<AppLoading/>
-  }
-  return (
-    <SafeAreaView style={{flex:1}}>
-      <StatusBar />
-      <Cesta {...mock}/>
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={{ flex: 1 }}>
+    <StatusBar />
+    <Home melhoresProdutores={false} />
+    {/* {produtores.length > 0 && 
+      <Cesta produtor={{
+        nome: produtores[0].nome, 
+        imagem: produtores[0].imagem
+      }}
+      {...produtores[0].cestas[0]} />
+    } */}
+  </SafeAreaView>
 }
-
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,41 +1,49 @@
 import React from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
-import Topo from './Componentes/Topo';
-import Detalhes from './Componentes/Detalhes';
-import Item from './Componentes/Item';
 import Texto from '../../componentes/Texto';
 
-export default function Cesta({topo, detalhes, itens}){
-    return <>
-        <FlatList
-            data={itens.lista}
-            renderItem={Item}
-            keyExtractor={({ nome }) => nome}
-            ListHeaderComponent={()=>{
-                return<>
-                 <Topo {...topo}/>
-                    <View style={estilos.cesta}>
-                        <Detalhes {...detalhes}/>
-                        <Texto style={estilos.titulo}> {itens.titulo}</Texto>
-                    </View>
-                </>
-            }}
-        />
-       
-    </>
+import Topo from '../../componentes/Topo';
+import useTextos from '../../hooks/useTextos';
+import Detalhes from './componentes/Detalhes';
+import Item from './componentes/Item';
+
+export default function Cesta({ detalhes, itens, produtor }) {
+  const { topoCesta, tituloItens } = useTextos();
+
+  return <>
+    <FlatList
+      data={itens}
+      renderItem={Item}
+      keyExtractor={({ nome }) => nome }
+      ListHeaderComponent={() => {
+        return <>
+          <Topo titulo={topoCesta} />
+          <View style={estilos.cesta}>
+            <Detalhes {...detalhes} produtor={produtor} />
+            <Texto style={estilos.titulo}>{ tituloItens }</Texto>
+          </View>
+        </>
+      }}
+      style={estilos.lista}
+    />
+  </>
 }
+
 const estilos = StyleSheet.create({
-    titulo:{
-        color:"#464646",
-        fontWeight:"bold",
-        marginTop:32,
-        marginBottom:8,
-        fontSize:20,
-    }, 
-    cesta:{
-        paddingVertical:8,
-        paddingHorizontal:16
-    },
-   
-})
+  lista: {
+    backgroundColor: '#ffffff',
+  },
+  titulo: {
+    color: "#464646",
+    fontWeight: "bold",
+    marginTop: 32,
+    marginBottom: 8,
+    fontSize: 20,
+    lineHeight: 32,
+  },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+});
